8cad503323d24c6c99a70b90269794b09a095220
Morten Hauan <morten@hauan.me>
2017-08-29 21:41:17 +0200

    Remove unused Links definition in App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import * as BooksAPI from './BooksAPI'
-import { Route, Link } from 'react-router-dom'
+import { Route } from 'react-router-dom'
 import SearchBooks from './SearchBooks'
 import ListBooks from './ListBooks'
 import './App.css'
